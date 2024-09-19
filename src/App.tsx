1bb6be8f73abaee4dddb8bba4ab832df9e215a36
T-Terra <gabst138@gmail.com>
2024-09-19 18:47:15 -0300

    feat: add logic of digit and clearMemory

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,72 @@
-import React, { useState } from "react"
+import React, { Component } from "react"
 import { SafeAreaView, Text, StyleSheet, View } from "react-native"
 import Button from "./components/Button"
 import Display from "./components/Display"
 
-export default () => {
-  const [displayValue, setDisplayValue] = useState(0)
+const initialState = {
+  displayValue: '0',
+  clearDisplay: false,
+  operation: null,
+  values: [0, 0],
+  current: 0,
+}
 
-  const addDigit = n => {
-    setDisplayValue(n)
+export default class App extends Component {
+  state = { ...initialState }
+
+  addDigit = n => {
+    if(n === '.' && this.state.displayValue.includes('.')) {
+      return
+    }
+
+    const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
+    const currentValue = clearDisplay ? '' : this.state.displayValue
+    const displayValue = currentValue + n
+    this.setState({displayValue, clearDisplay: false})
+
+    if(n !== '.') {
+      const newValue = parseFloat(displayValue)
+      const values = [...this.state.values]
+      values[this.state.current] = newValue
+      this.setState({ values })
+    }
   }
 
-  const clearMemory = () => {
-    setDisplayValue(0)
+  clearMemory = () => {
+    this.setState({ ...initialState })
   }
 
-  const setOperation = operation => {
+  setOperation = operation => {
 
   }
 
-  return (
-    <SafeAreaView style={style.buttons}>
-      <Display value={displayValue}/>
-      <View style={style.buttons}>
-        <Button label="AC" triple onClick={clearMemory}/>
-        <Button label="/" operation onClick={setOperation}/>
-        <Button label="7" onClick={addDigit}/>
-        <Button label="8" onClick={addDigit}/>
-        <Button label="9" onClick={addDigit}/>
-        <Button label="*" operation onClick={setOperation}/>
-        <Button label="4" onClick={addDigit}/>
-        <Button label="5" onClick={addDigit}/>
-        <Button label="6" onClick={addDigit}/>
-        <Button label="-" operation onClick={setOperation}/>
-        <Button label="1" onClick={addDigit}/>
-        <Button label="2" onClick={addDigit}/>
-        <Button label="3" onClick={addDigit}/>
-        <Button label="+" operation onClick={setOperation}/>
-        <Button label="0" double onClick={addDigit}/>
-        <Button label="." onClick={addDigit}/>
-        <Button label="=" operation onClick={setOperation}/>
-      </View>
-    </SafeAreaView>
-  )
+  render(): React.ReactNode {
+    return (
+      <SafeAreaView style={style.buttons}>
+        <Display value={this.state.displayValue}/>
+        <View style={style.buttons}>
+          <Button label="AC" triple onClick={this.clearMemory}/>
+          <Button label="/" operation onClick={this.setOperation}/>
+          <Button label="7" onClick={this.addDigit}/>
+          <Button label="8" onClick={this.addDigit}/>
+          <Button label="9" onClick={this.addDigit}/>
+          <Button label="*" operation onClick={this.setOperation}/>
+          <Button label="4" onClick={this.addDigit}/>
+          <Button label="5" onClick={this.addDigit}/>
+          <Button label="6" onClick={this.addDigit}/>
+          <Button label="-" operation onClick={this.setOperation}/>
+          <Button label="1" onClick={this.addDigit}/>
+          <Button label="2" onClick={this.addDigit}/>
+          <Button label="3" onClick={this.addDigit}/>
+          <Button label="+" operation onClick={this.setOperation}/>
+          <Button label="0" double onClick={this.addDigit}/>
+          <Button label="." onClick={this.addDigit}/>
+          <Button label="=" operation onClick={this.setOperation}/>
+        </View>
+      </SafeAreaView>
+    )
+  }
+
 }
 
 const style = StyleSheet.create({
